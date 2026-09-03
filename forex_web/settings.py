96b47cef02fa04fd3b9fa924a8011daf34203528
pratik <pratikparmar477@gmail.com>
2026-09-03 12:23:07 +0530

    chnages for hosting on render

--- forex_web/settings.py
+++ forex_web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1$imue9gbj2dx!s!b&&9ez6%8bg+vo1qn)*ab19a61mu+!)g*)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,3 +127,5 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
